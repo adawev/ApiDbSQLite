586420d5f7f04386082d3f0b71131b076c807bee
Diyor Adashev <adashevdiyor443@gmail.com>
2025-10-01 15:20:19 +0500

    CR full working with Front

--- app.py
+++ app.py
@@ -3,21 +3,24 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
+
 # Database connection
-con = sqlite3.connect("users.db")
+con = sqlite3.connect("users.db", check_same_thread=False)
 cur = con.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS usersAll (firstName text, lastName text, phone text)")
 con.commit()
-
-app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
 
 
 @app.route("/api/users" , methods=['GET'])
 def get_users():
     cur.execute("SELECT * FROM usersAll")
     users = cur.fetchall()
+
+    users_list = [{"firstName": u[0], "lastName": u[1], "phone": u[2]} for u in users]
+    return jsonify(users_list), 200
 
 @app.route("/api/users", methods=['POST'])
 def create_user():
@@ -27,7 +30,7 @@ def create_user():
     lastName = data.get("lastName")
     phone = data.get("phone")
 
-    cur.execute("INSERT INTO usersAll(first_name, last_name, phone) VALUES (?, ?, ?)", (firstName, lastName, phone))
+    cur.execute("INSERT INTO usersAll(firstName, lastName, phone) VALUES (?, ?, ?)",(firstName, lastName, phone))
     con.commit()
 
     return jsonify({
